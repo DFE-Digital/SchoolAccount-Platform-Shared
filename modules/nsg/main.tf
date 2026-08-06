fadabc1860a7a90d1f6ba5c783245cb27ecf37e5
Matt Archer <matthew.archer@education.gov.uk>
2026-08-06 13:10:38 +0100

    tags update

--- modules/nsg/main.tf
+++ modules/nsg/main.tf
@@ -4,4 +4,8 @@ resource "azurerm_network_security_group" "this" {
   name                = each.value.name
   location            = var.location
   resource_group_name = var.resource_group_name
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
 }
